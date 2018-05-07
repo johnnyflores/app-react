796f1f82769bbee0781d696b3c72d25c87f4f17e
johnny <johnnyflores@live.ca>
2018-05-07 13:26:32 -0400

    Create components header, articles and banner

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
+
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
+const Menu = (props) => (
+    <IconMenu
+      {...props}
+      iconButtonElement={
+        <IconButton><MoreVertIcon /></IconButton>
+      }
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+    >
+    <MenuItem primaryText="Home" />
+    <MenuItem primaryText="Sign in" />
+    <MenuItem primaryText="Sign out" />
+    </IconMenu>
+);
+
+Menu.muiName = 'IconMenu';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div className="Header">
+            <MuiThemeProvider>
+                <div>
+                    <AppBar
+                        title="App React"
+                        iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+                        iconElementRight={<Menu />}
+                    />
+                </div>
+            </MuiThemeProvider>
+        </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import App from './components/App';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+
+const defaultState = {
+  appName: 'App React',
+  articles: null
+};
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
