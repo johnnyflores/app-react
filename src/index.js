fc7a79e5e36444079378071f46bab513870ff90a
johnny <johnnyflores@live.ca>
2018-05-08 18:50:30 -0400

    Insert middleware in index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = {
   appName: 'App React',
   articles: null
 };
 const reducer = function(state = defaultState, action) {
+  switch(action.type) {
+    case 'HOME_PAGE_LOADED':
+      return { ...state, articles: action.payload.articles };
+  }
   return state;
 };
-const store = createStore(reducer);
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render((
   <Provider store={store}>
